b5aac09a5c63804f42ea9a87050d721836944e8d
Sharon Yi <sharon@nexxt.in>
2025-09-22 12:02:33 -0400

    feat: add tooltip to CollapsibleContainer

---
+++ .changeset/stale-camels-trade.md
@@ -0,0 +1,5 @@
+---
+'inca-ui': patch
+---
+
+add tooltip to CollapsibleContainer
--- src/components/CollapsibleContainer/index.tsx
+++ src/components/CollapsibleContainer/index.tsx
@@ -1,17 +1,23 @@
 import * as React from 'react';
 import { Accordion, AccordionProps } from '@mantine/core';
+import Tooltip from '../ui/Tooltip';
+import Icon from '../ui/Icon';
 import classes from './CollapsibleContainer.module.css';
 
 export interface CollapsibleContainerProps extends AccordionProps {
     title: string;
     children: React.ReactNode;
     defaultOpen?: boolean;
+    tooltip?: string;
+    tooltipType?: 'info' | 'warning';
 }
 
 const CollapsibleContainer = ({
     title,
     children,
-    defaultOpen = true
+    defaultOpen = true,
+    tooltip,
+    tooltipType = 'info'
 }: CollapsibleContainerProps) => {
     const defaultValue = defaultOpen ? title : null;
 
@@ -29,8 +35,24 @@ const CollapsibleContainer = ({
             multiple={false}
             w={'100%'}
         >
-            <Accordion.Item value={title}>
-                <Accordion.Control>{title}</Accordion.Control>
+            <Accordion.Item value={title} data-tooltiptype={tooltipType}>
+                <Accordion.Control>
+                    {title}
+                    {tooltip && (
+                        <Tooltip label={tooltip}>
+                            {tooltipType === 'info' && (
+                                <Icon type="IconHelp" size="sm" color="gray" />
+                            )}
+                            {tooltipType === 'warning' && (
+                                <Icon
+                                    type="IconAlertTriangle"
+                                    size="sm"
+                                    color="var(--mantine-color-yellow-6)"
+                                />
+                            )}
+                        </Tooltip>
+                    )}
+                </Accordion.Control>
                 <Accordion.Panel>{children}</Accordion.Panel>
             </Accordion.Item>
         </Accordion>
