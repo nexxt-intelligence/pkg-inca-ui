7f04989c071b1f1e24e5000bc8747d43f869326c
Sharon Yi <sharon@nexxt.in>
2025-12-19 14:01:01 -0600

    fix: List overflow bug

---
+++ .changeset/warm-swans-float.md
@@ -0,0 +1,5 @@
+---
+'inca-ui': patch
+---
+
+fix: List overflow bug
--- src/components/ui/List/index.tsx
+++ src/components/ui/List/index.tsx
@@ -2,17 +2,27 @@ import {
     List as MantineList,
     ListProps as MantineListProps
 } from '@mantine/core';
-
+import classes from './List.module.css';
 export interface ListProps extends Omit<MantineListProps, 'children'> {
     items?: string[];
     children?: React.ReactNode;
 }
 
-const List = ({ items, children, ...props }: ListProps) => {
+const List = ({
+    items,
+    children,
+    withPadding,
+    listStyleType,
+    ...props
+}: ListProps) => {
     return (
         <MantineList
             spacing="sm"
-            styles={{ item: { marginTop: '0!important' } }}
+            classNames={{ root: classes.root, item: classes.item }}
+            withPadding={withPadding}
+            data-with-padding={withPadding}
+            listStyleType={listStyleType}
+            data-type={listStyleType}
             {...props}
         >
             {items
